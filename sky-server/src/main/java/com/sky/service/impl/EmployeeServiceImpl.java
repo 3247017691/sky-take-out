aff08ee4c2747aaddf43a816375688a608c443cf
xie123 <3247017691@qq.com>
2026-08-13 21:28:19 +0800

    feat(employee): 添加员工管理功能
    - 在EmployeeController中新增员工创建接口
    - 在EmployeeMapper中添加员工插入数据库方法
    - 在EmployeeService中定义新增员工服务接口
    - 在EmployeeServiceImpl中实现员工创建业务逻辑
    - 添加全局SQL约束违反异常处理机制
    - 集成EmployeeDTO数据传输对象支持员工信息传递

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +11,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -54,5 +59,32 @@ public class EmployeeServiceImpl implements EmployeeService {
         //3、返回实体对象
         return employee;
     }
+
+    @Override
+    public void add(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+
+        // 1.对象属性拷贝
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+        // 2.设置账号的状态：默认1为正常，0为锁定
+        employee.setStatus(StatusConstant.ENABLE);
+
+        // 3.设置密码为默认密码:123456,进行MD5加密
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+        //4.设置创建时间和更新时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //5.设置创建人和更新人
+        // TODO: 后续改为当前用户的ID
+        employee.setCreateUser(10L);
+        employee.setUpdateUser(10L);
+
+        //6.保存到数据库
+        employeeMapper.insert(employee);
+    }
+
 
 }
